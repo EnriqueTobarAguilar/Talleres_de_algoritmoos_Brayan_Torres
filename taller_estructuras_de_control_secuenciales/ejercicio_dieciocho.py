0ff6dd30f388c1b419a001b7a0150351a1103a62
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 22:01:17 -0500

    Ejercicio 18
    Solución ejercicio 18

---
+++ taller_estructuras_de_control_secuenciales/ejercicio_dieciocho.py
@@ -0,0 +1,5 @@
+#punto 18
+prestamo=float(input("escriba el valor del prestamo: "))
+intereses=float(input("escriba el valor pagado en intereses: "))
+razon=(intereses*100)/(prestamo*4)
+print(f"el porcientaje anual es: {razon}%")
