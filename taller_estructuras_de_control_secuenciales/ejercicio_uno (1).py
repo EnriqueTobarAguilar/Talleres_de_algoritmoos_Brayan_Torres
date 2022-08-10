ac2749986cc0621b130f66714d4e27df811bbd83
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 21:35:43 -0500

    Ejercicio 1
    Solución ejercicio 1

---
+++ taller_estructuras_de_control_secuenciales/ejercicio_uno (1).py
@@ -0,0 +1,8 @@
+edad_uno=input(("escriba la edad uno"))
+edad_dos=input(("escriba la edad dos"))
+edad_tres=input(("escriba la edad tres"))
+edad_uno=int(edad_uno)
+edad_dos=int(edad_dos)
+edad_tres=int(edad_tres)
+promedio=(edad_uno+edad_dos+edad_tres)/3
+print("el promedio es:",promedio)
