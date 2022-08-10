01d06caf04a67d89abf202f11d636483a5315a29
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 21:56:25 -0500

    Ejercicio 13
    Solución ejercicio 13

---
+++ taller_estructuras_de_control_secuenciales/ejercicio_trece.py
@@ -0,0 +1,11 @@
+#punto 13
+n1=int(input("cantidad de billetes de 50000 "))
+n2=int(input("cantidad de billetes de 20000 "))
+n3=int(input("cantidad de billetes de 10000 "))
+n4=int(input("cantidad de billetes de 5000 "))
+n5=int(input("cantidad de billetes de 2000 "))
+n6=int(input("cantidad de billetes de 1000 "))
+n7=int(input("cantidad de billetes de 500 "))
+n8=int(input("cantidad de billetes de 100 "))
+total=(n1*50000+n2*20000+n3*10000+n4*5000+n5*2000+n6*1000+n7*500+n8*100)
+print(f"cantidad de dinero en el banco: ${total}")
