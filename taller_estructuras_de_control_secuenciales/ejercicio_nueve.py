609189a298366f5406a0b25d63d3bd3cbba4b531
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 21:49:24 -0500

    Ejercicio 9
    Solución ejercicio 9

---
+++ taller_estructuras_de_control_secuenciales/ejercicio_nueve.py
@@ -0,0 +1,5 @@
+#punto 9
+horas=int(input("escriba la cantidad de horas trabajadas: "))
+precio=float(input("escriba el precio de la hora trabajada: "))
+total=(horas*precio)-(horas*precio*0.2)
+print("el total del salario neto es:",total)
