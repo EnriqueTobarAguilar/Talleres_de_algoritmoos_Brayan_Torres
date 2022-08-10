93188b982afdf89cb5b91205bf080c4e5a8b6e9b
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 22:49:22 -0500

    Delete ejercicio_diecisieteeeeeeeee.py

--- taller_estructuras_de_control_secuenciales/ejercicio_diecisieteeeeeeeee.py
+++
@@ -1,8 +0,0 @@
-#punto 17
-presupuesto=(float(input("escriba el valor del presupuesto: ")))
-ginecologia=presupuesto*0.40
-traumatologia=presupuesto*0.30
-pediatria=presupuesto*0.30
-print("ginecologia:",ginecologia)
-print("traumatologia:",traumatologia)
-print("pediatria:",pediatria)
