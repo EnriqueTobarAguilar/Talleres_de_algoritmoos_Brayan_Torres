7069c9b85c9785e48527017713ab08ebb62b6afc
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 21:44:36 -0500

    Ejercicio 6
    Solución ejercicio 6

---
+++ taller_estructuras_de_control_secuenciales/ejercicio_seis.py
@@ -0,0 +1,7 @@
+#punto 6
+Hombres=int(input("escriba el número de hombres" ))
+Mujeres=int(input("escriba el número de mujeres" ))
+Porcentaje_hombres=(Hombres*100)/(Hombres+Mujeres)
+Porcentaje_mujeres=(Mujeres*100)/(Hombres+Mujeres)
+print ("el porcentaje de hombres es:", Porcentaje_hombres)
+print ("el porcentaje de mujeres es:", Porcentaje_mujeres)
