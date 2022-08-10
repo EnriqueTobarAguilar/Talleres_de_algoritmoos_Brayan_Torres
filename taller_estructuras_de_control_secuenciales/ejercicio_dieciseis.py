f4349aeb5c3d880a6d4cf72a55f145ac5be888c6
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-09 22:00:25 -0500

    Ejercicio 16
    Solución ejercicio 16

---
+++ taller_estructuras_de_control_secuenciales/ejercicio_dieciseis.py
@@ -0,0 +1,5 @@
+#punto 16
+cantidad_combustible=float(input("escriba la cantidad de combustible: "))
+cantidad_total=cantidad_combustible*3.785
+pago=cantidad_total*50000
+print("total a pagar:",pago)
