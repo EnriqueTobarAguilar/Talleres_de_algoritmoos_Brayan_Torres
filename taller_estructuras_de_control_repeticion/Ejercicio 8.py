426d30ff4aa8a6592c740df7dc9022e7433ead08
Jaime Enrique Tobar Aguilar <109982520+EnriqueTobarAguilar@users.noreply.github.com>
2022-08-21 21:14:37 -0500

    Ejercicio 8
    Solución ejercicio 8

---
+++ taller_estructuras_de_control_repeticion/Ejercicio 8.py
@@ -0,0 +1,18 @@
+"""
+entradas
+contraseña-->c-->int
+
+salidas
+acceso permitido-->str
+clave invalida-->str
+"""
+while True:
+    c=int(input(""))  
+  #entradas
+    if(c==2002):
+        print("Acesso Permitido")  
+      #salidas
+        break
+    else:
+        print("Senha Invalida")  
+#salidas
